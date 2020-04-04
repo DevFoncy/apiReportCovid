71068c99275b5ca916ed9152d52ae36e73fc91c0
steveariasarias <steve.arias@unmsm.edu.pe>
2020-04-03 22:34:54 -0500

    fix name variables .env

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'testApiNode',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   multipleStatements: true
 });
 
